71a3ac79c975ee85fed84c920fe55a7c5f02f0d8
BSTluo <1946831552@qq.com>
2024-06-27 16:34:22 +0800

    更新版本

--- src/index.ts
+++ src/index.ts
@@ -9,42 +9,112 @@ export interface Config { }
 
 export const Config: Schema<Config> = Schema.object({});
 
-export function apply(ctx: Context) {
+export function apply(ctx: Context)
+{
   if (!ctx.word) { return; }
 
   // 进入房间事件
-  ctx.on('iirose/joinRoom', async session => {
+  ctx.on('iirose/joinRoom', async session =>
+  {
     if (!session.content) { return; }
     if (session.userId == session.bot.user.id) { return; }
 
     session.content = '加入房间公屏';
-    await ctx.word.driver.start(session, str => {
+    await ctx.word.driver.start(session, str =>
+    {
       if (!str) { return; }
       session.bot.sendMessage('public:', str);
     });
 
     session.content = '加入房间私聊';
-    await ctx.word.driver.start(session, str => {
+    await ctx.word.driver.start(session, str =>
+    {
       if (!str) { return; }
       session.bot.sendMessage(`private:${session.userId}`, str);
     });
+
+    session.content = `${session.userId}加入房间公屏`;
+    await ctx.word.driver.start(session, str =>
+    {
+      if (!str) { return; }
+      session.bot.sendMessage('public:', str);
+    });
+
+    session.content = `${session.userId}加入房间私聊`;
+    await ctx.word.driver.start(session, str =>
+    {
+      if (!str) { return; }
+      session.bot.sendMessage(`private:${session.userId}`, str);
+    });
+
+    if (session.userId.startsWith('X'))
+    {
+      session.content = '游客加入房间公屏';
+      await ctx.word.driver.start(session, str =>
+      {
+        if (!str) { return; }
+        session.bot.sendMessage('public:', str);
+      });
+
+      session.content = '游客加入房间私聊';
+      await ctx.word.driver.start(session, str =>
+      {
+        if (!str) { return; }
+        session.bot.sendMessage(`private:${session.userId}`, str);
+      });
+    }
   });
 
   // 离开房间事件
-  ctx.on('iirose/leaveRoom', async session => {
+  ctx.on('iirose/leaveRoom', async session =>
+  {
     if (!session.content) { return; }
     if (session.userId == session.bot.user.id) { return; }
 
     session.content = '退出房间公屏';
-    await ctx.word.driver.start(session, str => {
+    await ctx.word.driver.start(session, str =>
+    {
       if (!str) { return; }
       session.bot.sendMessage('public:', str);
     });
 
     session.content = '退出房间私聊';
-    await ctx.word.driver.start(session, str => {
+    await ctx.word.driver.start(session, str =>
+    {
       if (!str) { return; }
       session.bot.sendMessage(`private:${session.userId}`, str);
     });
+
+    session.content = `${session.userId}退出房间公屏`;
+    await ctx.word.driver.start(session, str =>
+    {
+      if (!str) { return; }
+      session.bot.sendMessage('public:', str);
+    });
+
+    session.content = `${session.userId}退出房间私聊`;
+    await ctx.word.driver.start(session, str =>
+    {
+      if (!str) { return; }
+      session.bot.sendMessage(`private:${session.userId}`, str);
+    });
+
+
+    if (session.userId.startsWith('X'))
+    {
+      session.content = '游客退出房间公屏';
+      await ctx.word.driver.start(session, str =>
+      {
+        if (!str) { return; }
+        session.bot.sendMessage('public:', str);
+      });
+
+      session.content = '游客退出房间私聊';
+      await ctx.word.driver.start(session, str =>
+      {
+        if (!str) { return; }
+        session.bot.sendMessage(`private:${session.userId}`, str);
+      });
+    }
   });
 }
